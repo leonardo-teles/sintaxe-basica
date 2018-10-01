832a1acea8940a31a22512a2bc48f189fd2af936
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2018-10-01 18:03:50 -0300

    Testando Variáveis

---
+++ src/TestaVariaveis.java
@@ -0,0 +1,16 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		System.out.println("Novo teste");
+	
+		int idade = 37;
+		System.out.println("A idade é " + idade);
+		
+		idade = 30 + 10;
+		System.out.println("A idade é " + idade);
+		
+		idade = (7 * 5) + 2;
+		System.out.println("A idade é " + idade);
+	}
+}
